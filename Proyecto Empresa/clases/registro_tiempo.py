a3d412dc4d6daca7a9e59a0e6fab19af2c11c87d
Manix <victor.moncada001@gmail.com>
2024-10-09 16:03:39 -0300

    clase registro tiempo

---
+++ Proyecto Empresa/clases/registro_tiempo.py
@@ -0,0 +1,8 @@
+class registro_tiempo():
+    def __init__(self,id_registro_tiempo,fecha,cantidad_horas_trabajadas,descripcion_registro,dia_extraordinario,comentario_registro):
+        self.id_registro_tiempo = id_registro_tiempo
+        self.fecha = fecha
+        self.cantidad_horas_trabajadas = cantidad_horas_trabajadas
+        self.descripcion_registro = descripcion_registro
+        self.dia_extraordinario = dia_extraordinario
+        self.comentario_registro = comentario_registro
